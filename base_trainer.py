bfa260fdb20dd60fd7df302b651d515df110c17f
Sam Russell <sam.h.russell@gmail.com>
2017-07-04 21:30:00 +1200

    First pass

---
+++ base_trainer.py
@@ -0,0 +1,132 @@
+# Code (c) Sam Russell 2017
+import pandas
+import argparse
+import numpy as np
+import keras
+from keras import backend as K
+from keras.utils import to_categorical
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+
+class BaseTrainer:
+  batch_size = 128
+  num_classes = 10
+  epochs = 1#2
+  validation_percentage = 0.99
+  img_rows = 28
+  img_cols = 28
+  img_channels = 1
+
+  def __init__(self):
+    # set up commandline arguments
+    #self.parser = argparse.ArgumentParser()
+    #self.parser.add_argument('--train', help='training data CSV', required=True)
+    #self.parser.add_argument('--test', help='test data CSV', required=True)
+    #self.parser.add_argument('--output', help='output data CSV', required=True)
+    pass
+
+  def run(self):
+    #self.load_args()
+    self.load_mnist_data()
+    shaped_values, shaped_labels = self.load_training_data()
+    testing_values, testing_labels = self.load_testing_data()
+    training_values, validation_values = self.split_data(shaped_values)
+    training_labels, validation_labels = self.split_data(shaped_labels)
+
+    print('values shape:', shaped_values.shape)
+    print(training_values.shape[0], 'training samples')
+    print(validation_values.shape[0], 'validation samples')
+
+    self.build_models(input_shape=training_values.shape[1:])
+
+    # training
+    # do this in a loop
+    num_samples = training_values.shape[0]
+    num_fakes = int(num_samples / self.num_classes)
+
+    for _ in xrange(self.epochs):
+      fake_values = np.random.uniform(0,1,size=[num_fakes,100])
+      fake_labels = to_categorical(np.full((num_fakes, 1), self.num_classes), self.num_classes+1)
+      self.real_image_model.fit(training_values, training_labels,
+                batch_size=self.batch_size,
+                epochs=1,
+                verbose=1,
+                validation_data=(validation_values, validation_labels))
+      self.fake_image_model.fit(fake_values, fake_labels,
+                batch_size=self.batch_size,
+                epochs=1,
+                verbose=1)
+
+    # save some samples
+    fake_values = np.random.uniform(0,1,size=[16,100])
+    images = self.generator.predict(fake_values)
+    plt.figure(figsize=(10,10))
+
+    for i in range(images.shape[0]):
+      plt.subplot(4, 4, i+1)
+      image = images[i, :, :, :]
+      image = np.reshape(image, [self.img_rows, self.img_cols])
+      plt.imshow(image, cmap='gray')
+      plt.axis('off')
+    plt.tight_layout()
+
+    plt.savefig("test_images.png")
+    plt.close('all')
+
+    #self.test_results(testing_values, testing_labels)
+
+  #def test_results(self, testing_values, testing_labels):
+    #predictions = self.model.predict(testing_values)
+    #df = pandas.DataFrame(data=np.argmax(predictions, axis=1), columns=['Label'])
+    #df.insert(0, 'ImageId', range(1, 1 + len(df)))
+
+    # save results
+    #df.to_csv(self.commandline_args.output, index=False)
+
+  #def load_args(self):
+  #  self.commandline_args = self.parser.parse_args()
+
+  def load_mnist_data(self):
+    self.mnist_data = mnist.load_data()
+
+  def load_training_data(self):
+    #training_dataframe = pandas.read_csv(self.commandline_args.train)
+    #values = training_dataframe.values[:,1:]
+    #labels = training_dataframe.values[:,0]
+    (X_train, y_train), (X_test, y_test) = self.mnist_data
+    
+    shaped_labels = to_categorical(y_train, self.num_classes+1)
+    scaled_values = self.scale_values(X_train)
+    shaped_values = self.reshape_values(scaled_values)
+
+    return shaped_values, shaped_labels
+
+  def load_testing_data(self):
+    #testing_dataframe = pandas.read_csv(self.commandline_args.test)
+    #values = testing_dataframe.values
+    
+    (X_train, y_train), (X_test, y_test) = self.mnist_data
+    shaped_labels = to_categorical(y_test, self.num_classes+1)
+    scaled_values = self.scale_values(X_test)
+    shaped_values = self.reshape_values(scaled_values)
+
+    return shaped_values, shaped_labels
+
+  def scale_values(self, values):
+    return values.astype('float32') / 255
+
+  def reshape_values(self, values):
+    # TODO make it work when data comes pre-shaped
+    if K.image_data_format() == 'channels_first':
+        reshaped_values = values.reshape(values.shape[0], self.img_channels, self.img_rows, self.img_cols)
+    else:
+        reshaped_values = values.reshape(values.shape[0], self.img_rows, self.img_cols, self.img_channels)
+
+    return reshaped_values
+
+  def split_data(self, data):
+    landmark = int(data.shape[0] * self.validation_percentage)
+    return data[:landmark], data[landmark:]
+
+  def build_models(self, input_shape):
+    raise NotImplementedError("Must be implemented by subclass")
---
+++ ss_gan_trainer.py
@@ -0,0 +1,70 @@
+# Implementation of Semi-Supervised Learning with Generative Adversarial Networks by Augustus Odena
+# https://arxiv.org/pdf/1606.01583.pdf
+# Code (c) Sam Russell 2017
+
+import base_trainer
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D, UpSampling2D
+from keras.layers import Input
+from keras.layers.core import Activation, Reshape
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import Adam
+from keras.activations import *
+import keras
+
+class SsGanTrainer(base_trainer.BaseTrainer):
+  def build_models(self, input_shape):
+    self.discriminator = Sequential()
+    self.discriminator.add(Conv2D(64, (5, 5), strides=(2, 2), padding = 'same', activation='relu', input_shape=input_shape))
+    self.discriminator.add(LeakyReLU(0.2))
+    self.discriminator.add(Dropout(0.5))
+    self.discriminator.add(Conv2D(128, (5, 5), strides=(2, 2), padding = 'same', activation='relu'))
+    self.discriminator.add(LeakyReLU(0.2))
+    self.discriminator.add(Dropout(0.5))
+    self.discriminator.add(Conv2D(256, (5, 5), strides=(2, 2), padding = 'same', activation='relu'))
+    self.discriminator.add(LeakyReLU(0.2))
+    self.discriminator.add(Dropout(0.5))
+    # 7x7 for MNIST
+    #H = Conv2D(512, (5, 5), strides=(2, 2), padding = 'same', activation='relu')(H)
+    #H = LeakyReLU(0.2)(H)
+    #H = Dropout(0.5)(H)
+    self.discriminator.add(Flatten())
+    self.discriminator.add(Dense(1+self.num_classes,activation='softmax'))
+    self.discriminator.summary()
+
+    self.generator = Sequential()
+    self.generator.add(Dense(7*7*256, input_shape=(100,)))
+    self.generator.add(BatchNormalization())
+    self.generator.add(Activation('relu'))
+    if keras.backend.image_data_format() == 'channels_first':
+        self.generator.add(Reshape([256, 7, 7]))
+    else:    
+        self.generator.add(Reshape([7, 7, 256]))
+    self.generator.add(Dropout(0.5))
+    self.generator.add(UpSampling2D(size=(2, 2)))
+    self.generator.add(Conv2D(128, (5, 5), padding='same'))
+    self.generator.add(BatchNormalization())
+    self.generator.add(Activation('relu'))
+    self.generator.add(Dropout(0.5))
+    self.generator.add(UpSampling2D(size=(2, 2)))
+    self.generator.add(Conv2D(256, (5, 5), padding='same'))
+    self.generator.add(BatchNormalization())
+    self.generator.add(Activation('relu'))
+    # we're ignoring input shape - just assuming it's 7,7,1
+    self.generator.add(Conv2D(1, (5, 5), padding='same'))
+    self.generator.add(Activation('sigmoid'))
+    self.generator.summary()
+
+    self.real_image_model = Sequential()
+    self.real_image_model.add(self.discriminator)
+    self.real_image_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=1e-4))
+
+    self.fake_image_model = Sequential()
+    self.fake_image_model.add(self.generator)
+    self.fake_image_model.add(self.discriminator)
+    self.fake_image_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=1e-4))
+
+if __name__ == "__main__":
+  SsGanTrainer().run()
